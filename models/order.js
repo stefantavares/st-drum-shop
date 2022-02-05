6c5f4052b00d95d45163652f1dfa39557ea3deff
Stefan Tavares <stefantavares@Stefans-MacBook-Pro-2.local>
2022-02-04 22:37:05 -0800

    Add LineItem Schema

--- models/order.js
+++ models/order.js
@@ -1,4 +1,13 @@
-const { Schema } = require("mongoose");
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const itemSchema = require('./itemSchema');
+
+const lineItemSchema = new Schema({
+    qty: { type: Number, default: 1 },
+    item: itemSchema
+  }, {
+    timestamps: true
+  });
 
 const orderSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User' },
